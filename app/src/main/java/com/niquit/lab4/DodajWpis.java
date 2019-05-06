d87331694570ef2679f39a86c81ad67b680c21a6
Nikita Florek <nikita.florek@home.com.pl>
2019-05-06 18:40:36 +0200

    adding sqlite

--- app/src/main/java/com/niquit/lab4/DodajWpis.java
+++ app/src/main/java/com/niquit/lab4/DodajWpis.java
@@ -10,6 +10,8 @@ import android.widget.Spinner;
 
 public class DodajWpis extends AppCompatActivity {
 
+    private int modyfi_id;
+
     public void wyslij (View view)
     {
 //        EditText kontrolka = (EditText)findViewById(R.id.editText); String pole = kontrolka.getText().toString();
@@ -22,6 +24,7 @@ public class DodajWpis extends AppCompatActivity {
         Spinner gatunek = (Spinner) findViewById(R.id.gatunek);
 
         Animal zwierze = new Animal(gatunek.getSelectedItem().toString(), kolor.getText().toString(), Float.valueOf(wielkosc.getText().toString()), opis.getText().toString());
+        zwierze.setId(this.modyfi_id);
 
         Intent intencja = new Intent();
         intencja.putExtra("nowy", zwierze);
@@ -40,5 +43,24 @@ public class DodajWpis extends AppCompatActivity {
                 new String[] {"Pies", "Kot", "Rybki"});
         Spinner gatunek = (Spinner) findViewById(R.id.gatunek);
         gatunek.setAdapter(gatunki);
+
+        Bundle extras = getIntent().getExtras();
+        try {
+            if(extras.getSerializable("element") != null) {
+                Animal zwierz = (Animal) extras.getSerializable("element");
+
+                EditText kolor = (EditText) findViewById(R.id.kolor);
+                EditText wielkosc = (EditText) findViewById(R.id.wielkosc);
+                EditText opis = (EditText) findViewById(R.id.opis);
+
+                kolor.setText(zwierz.getKolor());
+                wielkosc.setText(Float.toString(zwierz.getWielkosc()));
+                opis.setText(zwierz.getOpis());
+
+                this.modyfi_id=zwierz.getId();
+            }
+        } catch(Exception ex) {
+            this.modyfi_id = 0;
+        }
     }
 }
--- app/src/main/java/com/niquit/lab4/MainActivity.java
+++ app/src/main/java/com/niquit/lab4/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,8 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private MySQLite db = new MySQLite(this);
 
     @Override
-    protected void onActivityResult(
-            int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode==1 && resultCode==RESULT_OK) {
             Bundle extras = data.getExtras();
 //            String nowy = (String)extras.get("wpis"); target.add(nowy); adapter.notifyDataSetChanged();
@@ -29,7 +31,15 @@ public class MainActivity extends AppCompatActivity {
             this.db.dodaj(nowy);
             adapter.changeCursor(db.lista());
             adapter.notifyDataSetChanged();
-        } }
+        }
+        else if(requestCode==2 && resultCode==RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Animal nowy = (Animal) extras.get("nowy");
+            this.db.aktualizuj(nowy);
+            adapter.changeCursor(db.lista());
+            adapter.notifyDataSetChanged();
+        }
+    }
 
     public void nowyWpis(MenuItem mi)
     {
@@ -66,5 +76,16 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listview = (ListView) findViewById( R.id.listView );
         listview.setAdapter(this.adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int pos, long id) {
+                TextView name = (TextView) view.findViewById(android.R.id.text1);
+                Animal zwierz = db.pobierz(Integer.parseInt(name.getText().toString()));
+                Intent intencja = new Intent (getApplicationContext(), DodajWpis.class);
+                intencja.putExtra("element", zwierz);
+                startActivityForResult(intencja, 2);
+            }
+        });
     }
 }
